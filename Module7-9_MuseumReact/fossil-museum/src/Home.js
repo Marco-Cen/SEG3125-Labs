bb79c8a8feffda025a1745f9332024bb1ca4cb5d
Marco-Cen <marco.c3n@gmail.com>
2021-07-19 22:16:48 -0400

    New Home Page redesigned

--- Module7-9_MuseumReact/fossil-museum/src/Home.js
+++ Module7-9_MuseumReact/fossil-museum/src/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'; //Bc downloaded bootstrap? (React-bootstrap)
 // import bkgImage from './assets/titlePagebackground.jpeg';
 // import bkgImage2 from './assets/titlePagebackground2.jpeg';
-import image3 from './assets/backdrop.jpeg';
+import image3 from './assets/homeBkgImage.jpeg';
 // import image4 from './assets/carnivoreFossilBkg1.jpeg';
 import foregroundImg from './assets/titlePageForeground.png';
 import logo from './assets/FossilizedEngTrans.png';
@@ -22,15 +22,28 @@ const Home = () => {
 
       <div class="parallax">
 
+        {/* Company Logo */}
+        <div class="parallax__layer parallax__layer__4">
+          <img src={logo} alt="Fossilized" style={{ 
+            width:'500px'
+            }} />
+        </div>
+        
         {/* Trees */}
         <div class="parallax__layer parallax__layer__6">
-          <img src={foregroundImg} alt="foregroundImage" />
+          <img src={foregroundImg} alt="foregroundImage" style={{ 
+            height:'890px',
+            width: '2257px',
+            }}/>
         </div>
 
         {/* BackDrop Green Jurassic Leaves */}
-        <div class="parallax__layer parallax__layer__2">
-          <img src={image3} alt="image3" />
-        </div>
+        {/* <div class="parallax__layer parallax__layer__2">
+          <img src={image3} alt="image3" style={{ 
+            height:'100vh',
+            width: '900px',
+            }}/>
+        </div> */}
 
         {/* <div class="parallax__layer parallax__layer__0">
           <img src={bkgImage} alt="bkgImage" />
@@ -44,12 +57,6 @@ const Home = () => {
           <img src={image4} alt="bkgImage4" />
         </div> */}
 
-        {/* Company Logo */}
-        <div class="parallax__layer parallax__layer__4">
-          <img src={logo} alt="Fossilized" style={{ 
-            width:'500px'
-            }} />
-        </div>
         
 
         
